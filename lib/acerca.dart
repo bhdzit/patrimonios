698b4ec0cf665875b4802385ec0b09084da8dfae
you@example.com <you@example.com>
2023-04-16 13:12:50 -0600

    Correciones de advertencias

--- lib/acerca.dart
+++ lib/acerca.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: file_names, camel_case_types
+// ignore_for_file: file_names, camel_case_types, prefer_const_constructors, sort_child_properties_last, unnecessary_new
 
 import 'package:flutter/material.dart';
 
@@ -11,7 +11,13 @@ class acerca extends StatelessWidget {
       children: <Widget>[
         Container(
           height: 300,
-          child: Text(
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("assets/images/aboutx.png"),
+              fit: BoxFit.cover,
+            ),
+          ),
+          child: const Text(
             "Acerca de",
             textAlign: TextAlign.center,
             style: TextStyle(
@@ -20,14 +26,8 @@ class acerca extends StatelessWidget {
                 fontFamily: "Raleway",
                 fontWeight: FontWeight.w700),
           ),
-          decoration: const BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage("assets/images/aboutx.png"),
-              fit: BoxFit.cover,
-            ),
-          ),
         ),
-        Container(
+        SizedBox(
           height: 500,
           child: new SingleChildScrollView(
             child: Column(
@@ -50,7 +50,8 @@ class acerca extends StatelessWidget {
                   child: new Text(
                     ' El centro de información del patrimonio natural y cultural tiene como función principal ser un centro de monitoreo de las condiciones de dicho patrimonio, a partir de la generación de sinergias colectivas en las que se involucre a los actores sociales en el reconocimiento, cuidado, preservación, disfrute etcétera de los espacios y/o prácticas que sean manifestaciones patrimoniales.',
                     textDirection: TextDirection.ltr,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 17),
                     textAlign: TextAlign.justify,
                   ),
                   padding: const EdgeInsets.all(10.0),
@@ -71,12 +72,11 @@ class acerca extends StatelessWidget {
                 ),
                 Padding(
                   child: new Text(
-                    ' El propósito del Centro de información del Patrimonio Cultural Y Natural Del Valle Del Mezquital, Hidalgo surge debido a la preocupación por el desconocimiento de algunos lugares culturales y naturales en el Valle del Mezquital' +
-                        'por parte de los mismos habitantes de esta zona; lo que eventualmente puede provocar la poca valoración y conservación de estos, sobre todo, una merma o tergiversación en las prácticas sociales y culturales que se realizan en torno a éstos. Con base en' +
-                        'esto, el objetivo del Observatorio es identificar su categoría y subcategoría, revalorarlos, reutilizarlos, se difundan y divulguen por medio de las Tecnologías de la Información para una mayor rapidez y con un mayor alcance y con' +
-                        'ello, promoverlos y proveerlos de un adecuado uso.',
+                    // ignore: prefer_adjacent_string_concatenation
+                    ' El propósito del Centro de información del Patrimonio Cultural Y Natural Del Valle Del Mezquital, Hidalgo surge debido a la preocupación por el desconocimiento de algunos lugares culturales y naturales en el Valle del Mezquitalpor parte de los mismos habitantes de esta zona; lo que eventualmente puede provocar la poca valoración y conservación de estos, sobre todo, una merma o tergiversación en las prácticas sociales y culturales que se realizan en torno a éstos. Con base enesto, el objetivo del Observatorio es identificar su categoría y subcategoría, revalorarlos, reutilizarlos, se difundan y divulguen por medio de las Tecnologías de la Información para una mayor rapidez y con un mayor alcance y conello, promoverlos y proveerlos de un adecuado uso.',
                     textDirection: TextDirection.ltr,
-                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 17),
+                    style: const TextStyle(
+                        fontWeight: FontWeight.bold, fontSize: 17),
                     textAlign: TextAlign.justify,
                   ),
                   padding: const EdgeInsets.all(10.0),
